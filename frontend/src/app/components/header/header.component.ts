a39ec829e0d6a553bffac122333b3243f0e74dfd
Alexander Betke <main@alexinabox.de>
2025-03-29 12:28:45 +0100

    feat: added persistent darkmode

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -14,7 +14,9 @@ export class HeaderComponent implements OnInit {
   items: MenuItem[] | undefined;
   isDarkMode: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor() {
+    this.applyDarkModePreference();
+  }
 
 
   async ngOnInit() {
@@ -92,28 +94,23 @@ export class HeaderComponent implements OnInit {
     this.applyLogoInvert();
   }
 
+  applyDarkModePreference() {
+    if (localStorage['theme'] === "dark") {
+      const element = document.querySelector('html');
+      if (element) {
+        element.classList.add('dark');
+        this.toggleDarkMode();
+      }
+    } else {
+      localStorage['theme'] = "light";
+    }
+  }
+
   toggleDarkMode() {
     const element = document.querySelector('html');
 
     if (element) {
       this.isDarkMode = element.classList.toggle('my-app-dark');
-
-
-      const logoImg = document.getElementById('logo-png');
-      if (logoImg instanceof HTMLImageElement) {
-        if (this.isDarkMode) {
-          logoImg.src = '/assets/logoToolifyDark.png';
-        } else {
-          logoImg.src = '/assets/logoToolifyLight.png';
-        }
-      }
-
-      const darkModeToggleButton = document.getElementById('darkModeToggleButton');
-      if (darkModeToggleButton) {
-        if (this.isDarkMode) {
-
-        }
-      }
     }
   }
 
